21decf49c088094565d74bb69e2e0fb122b1ebfe
xcix <Uskov@.LANIT>
2014-07-29 10:17:01 +0400

    octave/matlab compatibility added

--- numeric/fixed_point.m
+++ numeric/fixed_point.m
@@ -19,11 +19,11 @@ P(1) = p0; %root estimate
 process_plot=[p0 0];
 for (i=2:50)
    z = P(i-1);
-   P(i) = 2*(z - 1)^0.5
+   P(i) = 2*(z - 1)^0.5;
    process_plot=[process_plot; [z P(i)] ];
    process_plot=[process_plot; [P(i) P(i)] ];
    err = abs(P(i) - P(i-1));
    plot(z, P(i), 'r.');
 end;
 
-plot(process_plot(:,1), process_plot(:,2),'green');
+plot(process_plot(:,1), process_plot(:,2),'color','green');
